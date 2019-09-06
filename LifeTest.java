a64809c81af495efa55b61cdf7c6ff0898c0e573
Jeroen Resoort <jeroen.resoort@jdriven.com>
2019-09-06 10:55:56 +0200

    remove dependency on sun package

--- LifeTest.java
+++ LifeTest.java
@@ -1,29 +1,31 @@
-import sun.jvm.hotspot.utilities.Assert;
-
-
 public class LifeTest {
 
     private static final int TIMES = 1_000_000;
 
-    public static void main(String[] args)
-            throws java.lang.InterruptedException{
+    public static void main(String[] args) {
         Life life = new Life();
 
         life.drawWorld();
         loopTimes(life, TIMES);
         life.drawWorld();
-        Assert.that(!life.currentlyAlive(2,2),"expected dead");
-        Assert.that(life.currentlyAlive(3,3),"expected alive");
-        Assert.that(life.currentlyAlive(4,4),"expected alive");
-        Assert.that(!life.currentlyAlive(5,5),"expected dead");
+        if(life.currentlyAlive(2,2)){
+            throw new IllegalStateException("expected dead");
+        }
+        if(!life.currentlyAlive(3,3)){
+            throw new IllegalStateException("expected alive");
+        }
+        if(!life.currentlyAlive(4,4)){
+            throw new IllegalStateException("expected alive");
+        }
+        if(life.currentlyAlive(5,5)){
+            throw new IllegalStateException("expected dead");
+        }
 
         System.out.println("^^^^^ TEST PASSED ^^^^^");
-
     }
 
-    public static void loopTimes(Life life, int times) {
+    private static void loopTimes(Life life, int times) {
         for (int i = 0; i< times; i++) {
-            i++;
             life.nextGeneration();
             System.out.println("Generations  =  " + i);
         }
